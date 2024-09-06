354f083230d8a23eeec279b3e9d09bdb29b029f6
Eljan Peralta <eljan233@gmail.com>
2024-09-06 17:50:29 +0800

    fixed few problems

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -2,69 +2,63 @@ class Admin::ItemsController < ApplicationController
   before_action :set_item, only: %i[ show edit update destroy ]
   before_action :authenticate_user!
 
-  # GET /items or /items.json
-  def index
-    @items = Item.order(item_name: :asc)
-    @item = Item.new
-  end
-
-  # GET /items/1 or /items/1.json
-  def show
-  end
-
-  # GET /items/new
-  def new
-    @item = Item.new
-  end
-
-  # GET /items/1/edit
-  def edit
-  end
-
-  # POST /items or /items.json
-  def create
-    @item = Item.new(item_params)
-
-    respond_to do |format|
+    # GET /items or /items.json
+    def index
+      @items = Item.order(item_name: :asc)
+      @item = Item.new
+    end
+  
+    # GET /items/1 or /items/1.json
+    def show
+    end
+  
+    # GET /items/new
+    def new
+      @item = Item.new
+    end
+  
+    # GET /items/1/edit
+    def edit
+    end
+  
+    # POST /items or /items.json
+    def create
+      @item = Item.new(item_params)
+  
       if @item.save
-        format.html { redirect_to admin_items_url, notice: "Item was successfully created." }
-        format.json { render :show, status: :created, location: @item }
+        redirect_to admin_items_url, notice: "Item was successfully created."
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
+        # Render the form with validation errors
+        flash.now[:alert] = 'Failed to create item. Please check the errors below.'
+        render :new, status: :unprocessable_entity
       end
     end
-  end
-
-  # PATCH/PUT /items/1 or /items/1.json
-  def update
-    respond_to do |format|
+  
+    # PATCH/PUT /items/1 or /items/1.json
+    def update
       if @item.update(item_params)
-        format.html { redirect_to admin_items_url, notice: "Item was successfully updated." }
-        format.json { render :show, status: :ok, location: @item }
+        redirect_to admin_items_url, notice: "Item was successfully updated."
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
+        # Render the form with validation errors
+        flash.now[:alert] = 'Failed to update item. Please check the errors below.'
+        render :edit, status: :unprocessable_entity
       end
     end
-  end
-
-  # DELETE /items/1 or /items/1.json
-  def destroy
-    @item.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to admin_items_url, notice: "Item was successfully destroyed." }
-      format.json { head :no_content }
+  
+    # DELETE /items/1 or /items/1.json
+    def destroy
+      @item.destroy!
+  
+      redirect_to admin_items_url, notice: "Item was successfully destroyed."
     end
-  end
-
-  private
+  
+    private
+  
     # Use callbacks to share common setup or constraints between actions.
     def set_item
       @item = Item.find(params[:id])
     end
-
+  
     # Only allow a list of trusted parameters through.
     def item_params
       params.require(:item).permit(:item_name, :item_description, :price)
