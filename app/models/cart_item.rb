025e3009dc52e836c1b7756e3f772add7832430a
jbmendoza <johndale.mendoza@gmail.com>
2024-09-07 09:47:22 +0800

    Fix the Add to Cart button

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,5 +2,5 @@ class CartItem < ApplicationRecord
   belongs_to :cart
   belongs_to :item
   
-  validates :quantity, numericality: { greater_than: 0 }
+  validates :quantity, numericality: { greater_than_or_equal_to: 1 }
 end
