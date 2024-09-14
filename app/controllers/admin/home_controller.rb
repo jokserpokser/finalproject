e2511748922dbd553f16d65e6ac2a3b73ebfdec7
Eljan Peralta <eljan233@gmail.com>
2024-09-14 19:18:21 +0800

    Added dashboard in admin panel

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -2,7 +2,9 @@ module Admin
   class HomeController < ApplicationController
     before_action :authenticate_user!
     def index
-      @items = Item.all 
+      @item_count = ::Item.count
+      @transactions_count= Transaction.count
+      @users_count = User.where(user_type: "buyer").count
     end
   end
 end
